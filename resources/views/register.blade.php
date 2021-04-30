4849e136547710331b9765c23b217a83f4ade984
FedDiSte <33763790+FedDiSte@users.noreply.github.com>
2021-04-30 13:57:06 +0200

    Aggiornamenti su pagina registrazione

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,28 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="/register" method="post">
-        @csrf
-        <label for="nome">Nome: </label>
-        <input type="text" name="nome" id="nome"><br>
-        <label for="cognome">Cognome: </label>
-        <input type="text" name="cognome" id="cognome"><br>
-        <label for="email">Email: </label>
-        <input type="email" name="email" id="email"><br>
-        <label for="username">Username: </label>
-        <input type="text" name="username" id="username"><br>
-        <label for="password">Password: </label>
-        <input type="password" name="password" id="password"><br>
-        <input type="submit" value="Registrati">
-    </form>
-    @if ($status == 'email_not_unique')
-        <p>bell fratm</p>
-    @endif
-</body>
-</html>
+<x-master>
+    <x-slot name="show">
+        none
+    </x-slot>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 card">
+                <form action="/register" method="POST" class="form-floating">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingUsername" placeholder="Username" name="username">
+                        <label for="floatingUsername">Username</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com" name="email">
+                        <label for="floatingEmail">Email address</label>
+                      </div>
+                      <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                      </div>
+                </form>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
+</x-master>
